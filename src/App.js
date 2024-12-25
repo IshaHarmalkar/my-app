6667012847952ee9c6b5df27d60597b4f35da961
IshaHarmalkar <167905999+IshaHarmalkar@users.noreply.github.com>
2024-12-25 11:40:48 +0530

    Added bootstrap and updated package

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-
-import './App.css';
-
+import "./App.css";
 function App() {
   return (
     <div className="App">
-  Weather App
+      <div className="container">
+        <h1>Wetaher App</h1>
+        {/* <button className="btn btn-primary">Hello</button> */}
+        <footer>
+          This project was coded by Isha and is
+          <a
+            href="https://github.com/IshaHarmalkar/my-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            open sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
