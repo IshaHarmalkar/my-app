cde4d37a97262a30d7ec0a38980febd671b88b64
IshaHarmalkar <167905999+IshaHarmalkar@users.noreply.github.com>
2025-01-24 11:20:39 +0530

    Added wind

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -17,15 +17,19 @@ export default function WeatherInfo(props) {
         <div className="col-6">
           <div className="d-flex align-items-center">
             {/* <div className=""></div> */}
-            <WeatherIcon code={props.data.icon} alt={props.data.description} size={52}/>
+            <WeatherIcon
+              code={props.data.icon}
+              alt={props.data.description}
+              size={52}
+            />
             {/* <img
               //  src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAdVJREFUaN7tmc1thDAQRimBElwCJVBCSvAxR5fgEiiBEiiBErhyIx24A2cc2WhiAf4ZA1rJkZ4UZZPN9/AwHrON1rr5ZJoqUAWqQBWoAlWgxJf++WaAAGZAAdpD2dfM7zDS/yopAGE6YDoIHMLIdK8KQIAWGIAtQ8Bh/r59bQWQjCBILCkSJIF1XVuAA9Jivm9ROd0ukS0AQTtgA7SH+Vn31EoEBSAMA2YUUAHiJDyWcCtBuidIArZEroJewVEpjQSJjiIgMsMbpHdjf53sCcEWSxEYCQKOyZQhkshZBZYkYEtHeLVPQSGJnHIS0QI2/FIo+L+VILTXOUVA3BD+D3Q/pAqoFIEebUxFQQLJN/Ojo0TEqDG/JgBv1hdgeVNAP4CKPSvkCKiCQc1KSMRs2+x902hO/Z4cYFhgWOQHY8zo9hOKgCCGH71BEXcqHjEBKDft5gowypVH4YeLgKE9ZSO10cxz7z7TFJqxOEUgZxyYbPi+0M4uSRuZPYCnCPBA6TwrYCWWyFbJImo/FTMpM6pAG5CYvDO0LDii7x2JNAtdSGxuQyp41Q87UqkHW8NJzYsbw+8d6Y5Hi+7qbw8IyOIPd9HRVD8qUD8fqAJVoApUgSrwqfwCJ6xaZshM+xMAAAAASUVORK5CYII="
               src={props.data.iconUrl}
               alt={props.data.description}
             /> */}
             {/* <div> */}
-               <WeatherTemperature celsius={props.data.temperature} /> 
-              {/* <span className="temperature">
+            <WeatherTemperature celsius={props.data.temperature} />
+            {/* <span className="temperature">
                 {Math.round(props.data.temperature)}
               </span>
               <span className="unit">°C</span>  */}
@@ -36,7 +40,7 @@ export default function WeatherInfo(props) {
           <ul>
             <li>Percipitation : 55%</li>
             <li>Humidity : {props.data.humidity}%</li>
-            <li>Wind : {props.data.wind}</li>
+            <li>Wind : {props.data.wind.speed}</li>
           </ul>
         </div>
       </div>
